6d68314908d412670adef6f360ca09b5abdf315f
katakata0522 <tomokiandyuuki3@gmail.com>
2026-06-25 22:46:33 +0900

    refactor(kindle-tracker): header nav, premium submit btn, sort label, localized ad placeholder, shorter memo hint

--- kindle-tracker/sw.js
+++ kindle-tracker/sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'kindle-tracker-v';
-const CACHE_NAME = 'kindle-tracker-v20260625_2142';
+const CACHE_NAME = 'kindle-tracker-v20260625_2246';
 const ASSETS = [
   './',
   './index.html',
