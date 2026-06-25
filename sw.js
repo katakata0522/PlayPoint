f91ecb32ff422423a6eb9e0f7d1bd665534e9986
katakata0522 <tomokiandyuuki3@gmail.com>
2026-06-25 20:07:47 +0900

    build: synchronize asset versions and generate multi-language HTML outputs

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const CACHE_PREFIX = 'playpoint-calc-v';
-const CACHE_NAME = 'playpoint-calc-v20260625_1350';
+const CACHE_NAME = 'playpoint-calc-v20260625_2007';
 const ASSETS = [
   './',
-  './style.css?v=20260624_1752a',
+  './style.css?v=20260625_2007a',
   './favicon.svg',
   './ogp.png',
   './manifest.json',
@@ -24,9 +24,9 @@ const ASSETS = [
   './js/diary.js',
   './js/calculator.js',
   './js/share.js',
-  './js/main.js?v=20260624_1752a',
+  './js/main.js?v=20260625_2007a',
   './js/consent.js?v=20260619a',
-  './js/third-party.js?v=20260624_1752a',
+  './js/third-party.js?v=20260625_2007a',
   './blog/style.css?v=20260619a',
   './blog/components.js?v=20260621a',
   './blog/script.js?v=20260619a',
