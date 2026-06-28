ccfcb380d2d19175e6ddd0f08f82ed20824caf67
katakata0522 <tomokiandyuuki3@gmail.com>
2026-06-29 01:22:51 +0900

    refactor: optimize rendering and unify states in dashboard, sub health, and rakuten sim

--- kindle-tracker/sw.js
+++ kindle-tracker/sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'kindle-tracker-v';
-const CACHE_NAME = 'kindle-tracker-v20260629_0059';
+const CACHE_NAME = 'kindle-tracker-v20260629_0121';
 const ASSETS = [
   './',
   './index.html',
