766b8a5e20ef344b2fec2cd88252fb8b51756fcd
katakata0522 <tomokiandyuuki3@gmail.com>
2026-06-29 02:04:00 +0900

    feat: publish new blog article for savings game and Side FIRE roadmap

--- kindle-tracker/sw.js
+++ kindle-tracker/sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'kindle-tracker-v';
-const CACHE_NAME = 'kindle-tracker-v20260629_0121';
+const CACHE_NAME = 'kindle-tracker-v20260629_0203';
 const ASSETS = [
   './',
   './index.html',
--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const CACHE_PREFIX = 'playpoint-calc-v';
-const CACHE_NAME = 'playpoint-calc-v20260629_0121';
+const CACHE_NAME = 'playpoint-calc-v20260629_0203';
 const ASSETS = [
   './',
-  './style.css?v=20260629_0121a',
+  './style.css?v=20260629_0203a',
   './favicon.svg',
   './ogp.png',
   './manifest.json',
@@ -25,10 +25,10 @@ const ASSETS = [
   './js/diary.js',
   './js/calculator.js',
   './js/share.js',
-  './js/main.js?v=20260629_0121a',
-  './js/intent-tracking.js?v=20260629_0121a',
+  './js/main.js?v=20260629_0203a',
+  './js/intent-tracking.js?v=20260629_0203a',
   './js/consent.js?v=20260619a',
-  './js/third-party.js?v=20260629_0121a',
+  './js/third-party.js?v=20260629_0203a',
   './blog/style.css?v=20260619a',
   './blog/components.js?v=20260621a',
   './blog/script.js?v=20260628_1053a',
