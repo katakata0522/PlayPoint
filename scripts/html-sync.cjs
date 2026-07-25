c11cb7b59ea364975575c11d50d406867d7d9307
katakata0522 <tomokiandyuuki3@gmail.com>
2026-07-26 08:29:11 +0900

    CI再現性を修正: scripts/html-sync.cjs

--- scripts/html-sync.cjs
+++ scripts/html-sync.cjs
@@ -4,6 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const { replaceAssetVersion, replaceDateMetadata } = require('./html-replacements.cjs');
 
+const CONTENT_DATE_OVERRIDES = Object.freeze({
+  'en/articles/2026-06-20-discount-gift-cards.html': '2026-07-25',
+  'ko/articles/2026-06-20-discount-gift-cards.html': '2026-07-25',
+  'tw/articles/2026-06-20-discount-gift-cards.html': '2026-07-25'
+});
+
 function syncHtmlFile(rootDir, file, assetVersions, todayStr) {
   const filePath = path.join(rootDir, file);
   if (!fs.existsSync(filePath)) return false;
@@ -27,7 +33,7 @@ function syncHtmlFile(rootDir, file, assetVersions, todayStr) {
 
 function syncHtmlFiles(rootDir, files, assetVersions, todayStr) {
   for (const file of files) {
-    syncHtmlFile(rootDir, file, assetVersions, todayStr);
+    syncHtmlFile(rootDir, file, assetVersions, CONTENT_DATE_OVERRIDES[file] || todayStr);
   }
 }
 
