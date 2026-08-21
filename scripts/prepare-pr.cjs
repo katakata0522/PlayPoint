3af2f7f0a13f9b85a6de91f2f861cbaa3919499b
katakata0522 <tomokiandyuuki3@gmail.com>
2026-08-21 16:51:10 +0900

    refactor: use dependency-free locale ids in PR preparation

--- scripts/prepare-pr.cjs
+++ scripts/prepare-pr.cjs
@@ -3,7 +3,7 @@
 const { spawnSync } = require('node:child_process');
 const fs = require('node:fs');
 const path = require('node:path');
-const { createLocales } = require('./locale-config.cjs');
+const { INTERNATIONAL_LOCALES } = require('./locale-ids.cjs');
 
 const root = path.resolve(__dirname, '..');
 const OFFICIAL_SOURCE_PATTERN = /support\.google\.com\/googleplay|play\.google\.com\/store\/apps\/editorial/;
@@ -37,7 +37,7 @@ function committedBuildPins() {
 function scanOfficialSources() {
   const registry = JSON.parse(fs.readFileSync(path.join(root, 'blog', 'articles.json'), 'utf8'));
   const japanese = registry.map(article => String(article.file || '').replace(/^\.\.\//, ''));
-  const international = Object.keys(createLocales()).flatMap(locale => {
+  const international = INTERNATIONAL_LOCALES.flatMap(locale => {
     const directory = path.join(root, locale, 'articles');
     if (!fs.existsSync(directory)) return [];
     return fs.readdirSync(directory)
