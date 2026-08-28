04788dc05a2118d49b47f343f77aa0ddb1f85b52
katakata0522 <tomokiandyuuki3@gmail.com>
2026-08-28 20:07:03 +0900

    Preserve externally managed Manner Quiz deployment (#199)
    * fix: preserve externally managed manner deployment
    
    * test: enforce manner deployment ownership

--- .github/scripts/deploy-rsync.sh
+++ .github/scripts/deploy-rsync.sh
@@ -89,6 +89,7 @@ run_with_transient_retry() {
 deploy_once() {
   rsync -avz --delete-after --delete-excluded --delay-updates --timeout="$RSYNC_IO_TIMEOUT_SECONDS" \
     -e "$RSYNC_RSH" \
+    --filter='protect /manner/***' \
     ./ "$REMOTE_HOST:$REMOTE_ROOT/" \
     --exclude '/.git/***' \
     --exclude '/.github/***' \
