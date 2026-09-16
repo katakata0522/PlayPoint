16ed3cbd4555928e4e8cdbfe8112100443946b4b
katakata0522 <tomokiandyuuki3@gmail.com>
2026-09-16 10:43:59 +0900

    計算CTAと地域状態の配色を分離

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const CACHE_PREFIX = 'playpoint-calc-v';
-const CACHE_NAME = 'playpoint-calc-v20260912_1015-57c20420';
+const CACHE_NAME = 'playpoint-calc-v20260912_1015-0e46f3b2';
 // 初回は計算機の必須シェルだけを先読みし、記事・日記などは実利用時にキャッシュする。
 const ASSETS = [
   './',
-  './style.css?v=73a60935aa',
+  './style.css?v=fc18185aeb',
   './region-selector.css',
   './favicon.svg',
   './manifest.json',
