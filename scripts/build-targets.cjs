b01add2eaeefc09d2218676dec65d6e971592ec6
katakata0522 <tomokiandyuuki3@gmail.com>
2026-09-18 18:25:17 +0900

    refactor: build-targets.cjs の国際localeをSSOT化

--- scripts/build-targets.cjs
+++ scripts/build-targets.cjs
@@ -68,7 +68,7 @@ const generatedFiles = [
   'js/third-party.js',
   'blog/articles.json',
   'blog/article-search-index.json',
-  ...['en', 'ko', 'tw'].map(locale => `${locale}/articles/article-search-index.json`),
+  ...INTERNATIONAL_LOCALES.map(locale => `${locale}/articles/article-search-index.json`),
   ...articleHtmlFiles,
   ...Object.keys(EDITORIAL_TARGETS),
   ...syncedHtmlFiles,
