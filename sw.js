770a630368774b698ca7f130d7c46f72ddc1f7e9
katakata0522 <tomokiandyuuki3@gmail.com>
2026-09-19 19:54:38 +0900

    CSSアンミニファイ版の復元と生成物・サイトマップの再同期

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const CACHE_PREFIX = 'playpoint-calc-v';
-const CACHE_NAME = 'playpoint-calc-v20260912_1015-3da4e0e8';;
+const CACHE_NAME = 'playpoint-calc-v20260912_1015-b0a86bf3';;
 // 初回は計算機の必須シェルだけを先読みし、記事・日記などは実利用時にキャッシュする。
 const ASSETS = [
   './',
-  './style.css?v=0df4453c59',
+  './style.css?v=7065e3ea95',
   './region-selector.css',
   './favicon.svg',
   './manifest.json',
